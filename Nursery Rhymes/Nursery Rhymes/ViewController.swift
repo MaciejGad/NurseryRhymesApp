d2db5775dd755177ab2e184c2795a49aa6cc3b01
Maciej Gad <gad.maciej@gmail.com>
2020-12-06 21:07:02 +0100

    Basic app structure

---
+++ Nursery Rhymes/Nursery Rhymes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nursery Rhymes
+//
+//  Created by Maciej Gad on 06/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
